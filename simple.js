507bfc6b28810f094faa3c6dcd5d8d6053ac11db
François-David Collin <fradav@gmail.com>
2015-04-27 18:11:47 +0200

    commented simple.js

--- simple.js
+++ simple.js
@@ -1,6 +1,10 @@
-$(function() {
-    var title = $(".title").detach();
-    var postamble = $("#postamble").detach();
+$(function() {			// Apply only after loading DOM
+
+    ////////////////////////////////////////////////////////
+    // First Part, DOM handling
+    ////////////////////////////////////////////////////////
+    var title = $(".title").detach(); // Moving the title
+    var postamble = $("#postamble").detach(); // Moving the postamble
     if (title.length) {
 	var titlepage = $('<div/>',
 			  {id:'title',
@@ -8,6 +12,10 @@ $(function() {
 	titlepage = postamble.length ? titlepage.prepend(postamble) : titlepage;
 	$('#content').prepend(titlepage.prepend(title));
     }
+
+    ////////////////////////////////////////////////////////
+    // Second Part, Applying step and slide classes
+    ////////////////////////////////////////////////////////
     $("#title, #table-of-contents, .outline-2").each(function(i){
 	var custom_id = $(this).find(":first-child").attr("id");
 	if (custom_id != "unnumbered-"+(i+1))  {
@@ -16,18 +24,33 @@ $(function() {
 	$(this).addClass("step slide");
     });
 
-    var numSlides = $('.slide.step').length;
-    var slideWidth = $('.slide').width();
+    ////////////////////////////////////////////////////////
+    // Third Part, Applying transform rules by templates
+    ////////////////////////////////////////////////////////    
+    var numSlides = $('.slide.step').length; // How many slide do we
+					     // have?
+    var slideWidth = $('.slide').width();    // How wide are they ?
 
     var stepArray = new Array(numSlides);
     for	(i = 0; i < numSlides; i++) {
+	// The main idea is to make a rotating carrousel with the
+	// slides.  
     	stepArray[i] = {
+	    // We calculate the required radius of the disc,
     	    r:(numSlides*slideWidth*1.7)/(2*Math.PI),
+	    // using polar coordinates, place each slide at
+	    // corresponding angle
     	    phi:i*(360/numSlides),
-    	    rotate: { x:90, y:i*(360/numSlides) }
+	    // and rotate them at 90° on the x axis so their plane is
+	    // made perpendicular to the disc, and finally we rotate
+	    // them all on the y axis to make them tangent to the disc
+    	    rotate: { x:90,
+		      y:i*(360/numSlides) }
     	};
     }
+    // We apply the templates array to the slides
     $.jmpress("apply", '.slide', stepArray);
-    
+
+    // Finally, we launch the jmpress main().
     $('#content').jmpress();
 });
